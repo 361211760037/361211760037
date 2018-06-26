8241cc9da4b99d80cf10e20ef57142378e8ae78c
361211760037 <pndoil1421@gmail.com>
2018-06-26 11:40:04 +0700

    ็Hello World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,15 @@
+package lab1;
+
+public class myCode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+        System.out.println("น.ส.ปนัดดา สมพงศ์.");
+        System.out.println("361211760037.");
+        System.out.println("MIT211.");
+        System.out.println("ระบบสารสนเทศ.");
+
+
+
+    }//main
+}//class
